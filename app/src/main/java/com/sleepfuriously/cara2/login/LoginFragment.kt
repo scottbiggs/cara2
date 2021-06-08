be62603d61991bb97d7561c0f0f23ffacf07d41e
sbiggs <scott.biggs@consensus-builder.com>
2021-06-08 11:15:55 -0500

    Lots of changes: started removing the build-in login stuff (mostly from LoginFragment.kt). This required doing a lot of new functionality for getting the UI working just right.

--- app/src/main/java/com/sleepfuriously/cara2/login/LoginFragment.kt
+++ app/src/main/java/com/sleepfuriously/cara2/login/LoginFragment.kt
@@ -1,18 +1,17 @@
 package com.sleepfuriously.cara2.login
 
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
-import com.sleepfuriously.cara2.CameraFragmentDirections
 import com.sleepfuriously.cara2.CameraViewModel
 import com.sleepfuriously.cara2.MainActivity
 import com.sleepfuriously.cara2.databinding.FragmentLoginLayoutBinding
@@ -34,11 +33,14 @@ class LoginFragment : Fragment() {
     //  data
     //---------------------
 
-    private lateinit var loginViewModel: LoginViewModel
+//    private lateinit var loginViewModel: LoginViewModel
     private var _binding: FragmentLoginLayoutBinding? = null
 
     /** name of the user as they typed it in */
     private lateinit var mUserName : String
+
+    /** password as typed in by user */
+    private lateinit var mPassword : String
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -46,6 +48,16 @@ class LoginFragment : Fragment() {
 
     private lateinit var mCameraViewModel: CameraViewModel
 
+    //---------------------
+    //  widgets
+    //---------------------
+
+    private lateinit var mPassEt : EditText
+    private lateinit var mUserNameEt : EditText
+
+    private lateinit var mLoginButt : Button
+
+    private lateinit var mProgressBar: ProgressBar
 
     //---------------------
     //  overridden functions
@@ -65,62 +77,89 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Log.v(TAG, "onViewCreated()")
 
-        loginViewModel = ViewModelProvider(requireActivity(), LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
+//        loginViewModel = ViewModelProvider(requireActivity(), LoginViewModelFactory())
+//            .get(LoginViewModel::class.java)
+//
         mCameraViewModel = ViewModelProvider(requireActivity()).get((CameraViewModel::class.java))
 
         requireActivity().title = getString(R.string.login_frag_title)
 
-        val usernameEditText = binding.usernameEt
-        val passwordEditText = binding.password
-        val loginButton = binding.login
-        val loadingProgressBar = binding.loading
-
-        loginViewModel.loginFormState.observe(viewLifecycleOwner,
-            Observer { loginFormState ->
-                if (loginFormState == null) {
-                    return@Observer
-                }
-                loginButton.isEnabled = loginFormState.isDataValid
-                loginFormState.usernameError?.let {
-                    usernameEditText.error = getString(it)
-                }
-                loginFormState.passwordError?.let {
-                    passwordEditText.error = getString(it)
-                }
-            })
-
-        loginViewModel.loginResult.observe(viewLifecycleOwner,
-            Observer { loginResult ->
-                loginResult ?: return@Observer
-                loadingProgressBar.visibility = View.GONE
-                loginResult.error?.let {
-                    showLoginFailed(it)
-                }
-
-                loginResult.success?.let {
-                    // Successful login, yay!
-                    updateUiWithUser(it)
-                    saveDataToViewModel()
-
-                    Log.d(TAG, "about to go to Camera Fragment. view model username = ${mCameraViewModel.mNameLiveData.value.toString()}")
-
-                    // add some data to the navigation system
-                    findNavController().navigate(R.id.action_loginFragment_to_cameraFragment)
-                }
-            })
-
-        loginButton.setOnClickListener {
-            loadingProgressBar.visibility = View.VISIBLE
-            mUserName = usernameEditText.text.toString()
-            loginViewModel.login(
-                mUserName,
-                passwordEditText.text.toString()
-            )
-        }
+        mUserNameEt = binding.usernameEt
+        mPassEt = binding.passwordEt
+        mLoginButt = binding.login
+        mProgressBar = binding.loading
 
         (requireActivity() as MainActivity).setupCameraPermissions()
+
+        mLoginButt.setOnClickListener {
+            if (goodLoginEntries()) {
+                Log.d(TAG, "goodLoginEntries")
+                mProgressBar.visibility = View.VISIBLE
+
+                if (loginSuccessful()) {
+                    Log.d(TAG, "loginSuccessful")
+                    getDataFromUi()
+                    updateUiWithUser()
+                    saveDataToViewModel()
+                    // continue to CameraFragment
+                    findNavController().navigate(R.id.action_loginFragment_to_cameraFragment)
+                }
+                else {
+                    Log.d(TAG, "login NOT successful")
+                    // login problems
+                    userAlert(R.string.login_frag_unable_to_login)
+                    mProgressBar.visibility = View.GONE
+                }
+            }
+            else {
+                // problems with forms
+                Log.d(TAG, "NOT good login entries")
+                userAlert(R.string.login_frag_fill_in_forms)
+            }
+        }
+
+//        loginViewModel.loginFormState.observe(viewLifecycleOwner,
+//            Observer { loginFormState ->
+//                if (loginFormState == null) {
+//                    return@Observer
+//                }
+//                loginButton.isEnabled = loginFormState.isDataValid
+//                loginFormState.usernameError?.let {
+//                    usernameEditText.error = getString(it)
+//                }
+//                loginFormState.passwordError?.let {
+//                    passwordEditText.error = getString(it)
+//                }
+//            })
+//
+//        loginViewModel.loginResult.observe(viewLifecycleOwner,
+//            Observer { loginResult ->
+//                loginResult ?: return@Observer
+//                loadingProgressBar.visibility = View.GONE
+//                loginResult.error?.let {
+//                    showLoginFailed(it)
+//                }
+//
+//                loginResult.success?.let {
+//                    // Successful login, yay!
+//                    updateUiWithUser(it)
+//                    saveDataToViewModel()
+//
+//                    Log.d(TAG, "about to go to Camera Fragment. view model username = ${mCameraViewModel.mNameLiveData.value.toString()}")
+//
+//                    // add some data to the navigation system
+//                    findNavController().navigate(R.id.action_loginFragment_to_cameraFragment)
+//                }
+//            })
+
+//        loginButton.setOnClickListener {
+//            mUserName = usernameEditText.text.toString()
+//            loginViewModel.login(
+//                mUserName,
+//                passwordEditText.text.toString()
+//            )
+//        }
+
     } // onViewCreated()
 
 
@@ -134,19 +173,67 @@ class LoginFragment : Fragment() {
     //---------------------
 
     /**
+     * Returns TRUE iff all the login forms have at least something to check.
+     */
+    private fun goodLoginEntries() : Boolean {
+        if (mUserNameEt.text.isNotEmpty() && mPassEt.text.isNotEmpty()) {
+            return true
+        }
+        return false
+    }
+
+
+    /**
+     * Currently this is just a stub.
+     * todo: actually do some checks for valid account, etc.
+     */
+    private fun loginSuccessful() : Boolean {
+        return true
+    }
+
+    /**
+     * Moves data from ui elements into our class variables.
+     *
+     * side effects:
+     *  mUserName
+     *  mPassword
+     *
+     */
+    private fun getDataFromUi() {
+        mUserName = mUserNameEt.text.toString()
+        mPassword = mPassEt.text.toString()
+    }
+
+    /**
      * Does any UI changes once a successful login is completed.  In this case it's
      * simply display a toast.
      */
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome) + model.displayName
-        val appContext = context?.applicationContext ?: return
-        Toast.makeText(appContext, welcome, Toast.LENGTH_LONG).show()
+    private fun updateUiWithUser() {
+        val welcome = getString(R.string.welcome) + " " + mUserName + "!"
+        Toast.makeText(requireContext(), welcome, Toast.LENGTH_LONG).show()
     }
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        val appContext = context?.applicationContext ?: return
-        Toast.makeText(appContext, errorString, Toast.LENGTH_LONG).show()
+    /**
+     * Displays the given string
+     *
+     * @param   strId   id of the string to display
+     */
+    private fun userAlert(strId : Int) {
+        userAlert(getString(strId))
     }
+
+    /**
+     * Displays this string as an alert.
+     * Right now this is just a toast
+     */
+    private fun userAlert(str : String) {
+        Toast.makeText(requireContext(), str, Toast.LENGTH_LONG).show()
+    }
+
+//    private fun showLoginFailed(@StringRes errorString: Int) {
+//        val appContext = context?.applicationContext ?: return
+//        Toast.makeText(appContext, errorString, Toast.LENGTH_LONG).show()
+//    }
 
 
     /**
--- app/src/main/java/com/sleepfuriously/cara2/login/LoginViewModel.kt
+++ app/src/main/java/com/sleepfuriously/cara2/login/LoginViewModel.kt
@@ -37,28 +37,28 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
         }
     }
 
-//    fun loginDataChanged(username: String, password: String) {
-//        if (!isUserNameValid(username)) {
-//            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
-//        } else if (!isPasswordValid(password)) {
-//            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
-//        } else {
-//            _loginForm.value = LoginFormState(isDataValid = true)
-//        }
-//    }
+    fun loginDataChanged(username: String, password: String) {
+        if (!isUserNameValid(username)) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+        } else if (!isPasswordValid(password)) {
+            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+        } else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
+    }
 
-//    // A placeholder username validation check
-//    private fun isUserNameValid(username: String): Boolean {
-//        return if (username.contains("@")) {
-//            Patterns.EMAIL_ADDRESS.matcher(username).matches()
-//        } else {
-//            username.isNotBlank()
-//        }
-//    }
-//
-//    // A placeholder password validation check
-//    private fun isPasswordValid(password: String): Boolean {
-//        return password.length > 5
-//    }
+    // A placeholder username validation check
+    private fun isUserNameValid(username: String): Boolean {
+        return if (username.contains("@")) {
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        } else {
+            username.isNotBlank()
+        }
+    }
+
+    // A placeholder password validation check
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 5
+    }
 
 }
